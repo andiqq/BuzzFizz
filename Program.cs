c8d50048179e60d42474436289cfa9c0019dd215
Dr. Andreas Plagens <andreas@plagens.eu>
2020-04-02 21:57:49 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace BuzzFizz {
+    class Program {
+        static void Main(string[] args) 
+        {
+            for (int i = 1; i <= 50; i++) 
+            {
+                
+                bool writeNumber = true;
+
+                if (i % 3 == 0) 
+                {
+                    Write("Buzz");
+                    writeNumber = false;
+                }
+                if (i % 5 == 0) 
+                {
+                    Write("Fizz");
+                    writeNumber = false;
+                }
+                if (writeNumber) 
+                {
+                    Write(i);
+                }
+                Write(" ");
+            }
+        }
+    }
+}
